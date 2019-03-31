6fab94a72b4c32e63a35c9b54c4dc24f2cf56ac6
Brunowf <brunoweiller@hotmail.com>
2019-03-31 04:31:02 -0300

    Finalizando classes e associações de Lista e Tarefas

--- src/main/java/br/senai/sc/ListaDeTarefasApplication.java
+++ src/main/java/br/senai/sc/ListaDeTarefasApplication.java
@@ -1,13 +1,50 @@
 package br.senai.sc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ListaDeTarefasApplication {
+import br.senai.sc.domain.Lista;
+import br.senai.sc.domain.Tarefa;
+import br.senai.sc.repositories.ListaRepository;
+import br.senai.sc.repositories.TarefaRepository;
 
+@SpringBootApplication
+public class ListaDeTarefasApplication implements CommandLineRunner{
+
+	@Autowired
+	private ListaRepository listaRepo;
+	
+	@Autowired
+	private TarefaRepository tarefaRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ListaDeTarefasApplication.class, args);
 	}
-
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Lista l1 = new Lista(null, "Primeira lista");
+		
+		Tarefa i1 = new Tarefa(null, "Item 1", l1);
+		Tarefa i2 = new Tarefa(null, "Item 2", l1);
+		Tarefa i3 = new Tarefa(null, "Item 3", l1);
+		Tarefa i4 = new Tarefa(null, "Item 4", l1);
+		Tarefa i5 = new Tarefa(null, "Item 5", l1);
+		
+		l1.getTarefas().add(i1);
+		l1.getTarefas().add(i2);
+		l1.getTarefas().add(i3);
+		l1.getTarefas().add(i4);
+		l1.getTarefas().add(i5);
+		
+		listaRepo.save(l1);
+		tarefaRepo.save(i1);
+		tarefaRepo.save(i2);
+		tarefaRepo.save(i3);
+		tarefaRepo.save(i4);
+		tarefaRepo.save(i5);
+		
+	}
 }
